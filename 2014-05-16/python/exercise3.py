390a40ed686cdf8fb36b1348053f99c0f152dd6d
JITBALJINDER <bal.jit1590@gmail.com>
2014-05-17 18:43:14 +0200

    HW3

--- 2014-05-16/python/exercise3.py
+++ 2014-05-16/python/exercise3.py
@@ -12,50 +12,16 @@ from mapper import *
 from boolean import *
 from sysml import *
 '''***********************************************************'''
+from larcc import *
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
+def mergingNumberingElimination(diagramHole,diagram,toMerge,toRemove):
+	diagramHole = diagramHole[0],[cell for k,cell in enumerate(diagramHole[1]) if not (k in toRemove)]
+	toMerge = list(sort(toMerge))
+	iteratore = range(len(toMerge))
+	for i in iteratore:
+		elementMerge = toMerge[i]-i
+		diagram = diagram2cell(diagramHole,diagram,elementMerge)
+	return diagram
 
-master = assemblyDiagramInit([5,5,2])([[.3,3.2,.1,5,.3],[.3,4,.1,2.9,.3],[.3,2.7]])
-V,CV = master
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(CV)),CYAN,2)
-VIEW(hpc)
 
-toRemove = [13,33,17,37]
-master = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-DRAW(master)
 
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-VIEW(hpc)
-
-toMerge = 29
-cell = MKPOL([master[0],[[v+1 for v in  master[1][toMerge]]],None])
-VIEW(STRUCT([hpc,cell]))
-
-diagram = assemblyDiagramInit([3,1,2])([[2,1,2],[.3],[2.2,.5]])
-master = diagram2cell(diagram,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-VIEW(hpc)
-
-toRemove = [47]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-DRAW(master)
-
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-VIEW(hpc)
-
-toMerge = 34
-cell = MKPOL([master[0],[[v+1 for v in  master[1][toMerge]]],None])
-VIEW(STRUCT([hpc,cell]))
-
-diagram = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
-master = diagram2cell(diagram,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-VIEW(hpc)
-
-toRemove = [53,59]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-DRAW(master)
--- 2014-05-16/python/exercise4.py
+++ 2014-05-16/python/exercise4.py
@@ -12,4 +12,13 @@ from mapper import *
 from boolean import *
 from sysml import *
 '''***********************************************************'''
-
+def diagram2cell(diagram,master,cell):
+	mat = diagram2cellMatrix(diagram)(master,cell)
+	diagram =larApply(mat)(diagram)
+	V1,CV1 = master
+	CV1 = [x for y,x in enumerate(CV1) if y != cell]
+	V,CV1,CV2,n12 = vertexSieve((V1,CV1),diagram)
+	CV = CV1+CV2
+	master = V, CV
+	return master
+'''***********************************************************'''
