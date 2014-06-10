4184b823c40eff182bd1aab2a8301e45884e95eb
JITBALJINDER <bal.jit1590@gmail.com>
2014-06-10 15:15:07 +0200

    Hw

--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -2,6 +2,57 @@
 #Baljinder jit	477062
 from exercise1 import *
 from random import randint
+'''***********************************************************'''
+def generaAlbero(coordinate):
+	x,y,z = coordinate
+	def generaAlbero0(dimesioni):
+		r,h = dimesioni
+		tronco = larCylinder(r,h)([32,1])
+		albero = larSphere(r*4)()
+		tronco = COLOR(coloreTronco)(STRUCT(MKPOLS(tronco)))
+		albero = COLOR(coloreGiardino)(STRUCT(MKPOLS(albero)))
+		return T([1,2,3])([x,y,z])(STRUCT([tronco,T(3)(h+r)(albero)]))
+	return generaAlbero0
+'''***********************************************************'''
+def generaNuvola():
+	Nuv1 = [[1.62, 4], [1.75, 4.66], [2.01, 4.54], [2.32, 4.67]]
+	Nuv1 = MAP(Bezier(Nuv1))(Dom1D)
+	Nuv2 = [[1.62, 4], [0.05, 4.08], [0.43, 2.14], [2.16, 2.42]]
+	Nuv2 = MAP(Bezier(Nuv2))(Dom1D)
+	Nuv3 = [[2.85, 2.12], [2.57, 2.01], [2.22, 2.2], [2.16, 2.42]]
+	Nuv3 = MAP(Bezier(Nuv3))(Dom1D)
+	Nuv4 = [[2.85, 2.12], [2.81, 1.83], [4.36, 1.39], [4.76, 2.49]]
+	Nuv4 = MAP(Bezier(Nuv4))(Dom1D)
+	Nuv5 = [[5.51, 2.83], [5.52, 2.44], [5.14, 2.15], [4.76, 2.49]]
+	Nuv5 = MAP(Bezier(Nuv5))(Dom1D)
+	Nuv6 = [[5.51, 2.83], [6.84, 2.86], [6.6, 4.01], [5.67, 4.19]]
+	Nuv6 = MAP(Bezier(Nuv6))(Dom1D)
+	Nuv7 = [[4.31, 4.79], [5.12, 5.87], [6.1, 4.75], [5.67, 4.19]]
+	Nuv7 = MAP(Bezier(Nuv7))(Dom1D)
+	Nuv8 = [[4.31, 4.79], [4.42, 4.99], [3.68, 5.2], [3.67, 4.8]]
+	Nuv8 = MAP(Bezier(Nuv8))(Dom1D)
+	Nuv9 = [[2.32, 4.67], [2.65, 5.42], [3.24, 5.63], [3.67, 4.8]]
+	Nuv9 = MAP(Bezier(Nuv9))(Dom1D)
+	nuovola = STRUCT([Nuv1,Nuv2,Nuv3,Nuv4,Nuv5,Nuv6,Nuv7,Nuv8,Nuv9])
+	nuovola = SOLIDIFY(nuovola)
+	return MATERIAL([1,1,1,0, 0,0,0,0.2, 0,0,0,0, 0,0,0,0, 50])(nuovola)
+'''***********************************************************'''
+def spiral1(p):
+	alpha,r,h = p
+	return [r*COS(alpha), r*SIN(alpha), alpha/(2*PI)]
+'''***********************************************************'''
+
+def spiral2(p):
+    alpha,r,h = p
+    return [r*COS(alpha), r*SIN(alpha), alpha/(2*PI) + 0.2]
+dom1D = INTERVALS(1)(1)
+dom3D = INSR(PROD)([INTERVALS(36*PI)(140), dom1D, dom1D])
+'''***********************************************************'''
+Dom1D = INTERVALS(1)(10)
+Bezier = BEZIER(S1)
+'''***********************************************************'''
+
+
 appartamento1 = master
 master = assemblyDiagramInit([8,3,9])([[4,6,0.2,3,.2,9,.5,4],[4,8,8],[.5,4,.2,4,.2,4,.2,4,2]])
 #elimio celle inutili
